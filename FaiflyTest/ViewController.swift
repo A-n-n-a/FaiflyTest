fca8eb14b4b2df589dfd09be80c64f2e2b7f1b9f
A-n-n-a <alimanova1anna@gmail.com>
2017-07-11 20:35:30 +0300

    Fetch data from core data

--- FaiflyTest/ViewController.swift
+++ FaiflyTest/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var sortedCountries = [String]()
     var tempCities = ["Paris","Marcelle"] //, ["Madrid", "Barcelona"]]
     
+    var countriesFromCoreData = [String]()
+    var citiesFromCoreData = [[String]]() //, ["Madrid", "Barcelona"]]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,110 +57,50 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         let context = appDelegate.persistentContainer.viewContext
 
-        let entity = NSEntityDescription.insertNewObject(forEntityName: "Country", into: context)
+//        let entity = NSEntityDescription.insertNewObject(forEntityName: "Country", into: context)
+//        
+//        for i in countriesArray {
+//            entity.setValue(i.countryName, forKey: "name")
+//            entity.setValue(i.citiesList, forKey: "cities")
+//        }
+//        
+//        do {
+//            try context.save()
+//            print("SAVED")
+//        } catch {
+//            print("ERROR: \(error)")
+//        }
         
-        for i in countriesArray {
-            entity.setValue(i.countryName, forKey: "name")
-            entity.setValue(i.citiesList, forKey: "cities")
-        }
+        // getting data from core data
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Country")
+        request.returnsObjectsAsFaults = false
         
         do {
-            try context.save()
-            print("SAVED")
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results {
+                    if let countryName = (result as AnyObject).value(forKey: "name") as? String {
+                        //print(countryName)
+                        countriesFromCoreData.append(countryName)
+                        
+                    }
+                    if let citiesList = (result as AnyObject).value(forKey: "cities") as? [String] {
+                        //print(citiesList)
+                        citiesFromCoreData.append(citiesList)
+                        
+                    }
+                }
+            }
         } catch {
-            print("ERROR: \(error)")
+            print("Error: \(error)")
         }
-           //("France", forKeyPath: "name")
-//        country.setValue(["Paris", "Marcelle"], forKeyPath: "cities")  
-        //save(name: "Ukraine")
-        
-        //            let result = json[resultsString] as! [[String:AnyObject]]
-//            
-//            removeDataFromFirebase(isSearching: isSearching)
-//            
-//            for i in result {
-//                
-//                let singleRecipe = Recipe(dictionary: i)
-//                
-//                recipeArray.append(singleRecipe)
-//                
-//                let recipesItem = [
-//                    titleString: singleRecipe.title,
-//                    hrefString: singleRecipe.href,
-//                    ingredientsString: singleRecipe.ingredients,
-//                    thumbnailString: singleRecipe.recipeImage ?? " "
-//                    ] as [String : Any]
-//                
-//                saveDataToFirebase(text: recipesItem, isSearching: isSearching)
-            
-            //}
-        
-
-        
-        //JSON Open Weather Map with SwiftyJSON
-//        let session = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-//            
-//            if error != nil {
-//                print("Error: \(String(describing: error))")
-//            } else {
-//                
-//                
-//                
-//                let json = JSON(data!)
-//
-//                print(json)
-//                //                cityName = json[nameStr].stringValue
-//                //
-//                //                temp = json[mainStr][tempStr].double!
-//                //                let celcTemp = kelvinToCelcius(kelvin: temp)
-//                //                var tempString = emptyStr
-//                //                if celcTemp > 0 {
-//                //                    tempString = plusStr + String(celcTemp) + degreeStr
-//                //                } else {
-//                //                    tempString = String(celcTemp) + degreeStr
-//                //                }
-//                //                self.temperaturesArray.append(tempString)
-//                //
-//                //                latitude = json[coordStr][latStr].double!
-//                //                self.latitudesArray.append(latitude)
-//                //
-//                //                longtitude = json[coordStr][lonStr].double!
-//                //                self.longtitudesArray.append(longtitude)
-//                //
-//                //                description = json[weatherStr][0][descriptionStr].stringValue
-//                //                self.descriptionsArray.append(description)
-//                //
-//                //                icon = json[weatherStr][0][iconStr].stringValue
-//                //                //print("ICON: \(icon)")
-//                //                let iconImage = stringIconToImage(strIcon: icon)
-//                //                //print("ICON IMAGE: \(iconImage)")
-//                //
-//                //                self.weatherDetails = City(name: cityName, temp: tempString, desc: description, icon: iconImage, lon: longtitude, lat: latitude)
-//                //                self.cityList.append(self.weatherDetails!)
-//                //
-//                //                // Display weather on the main thread
-//                //                DispatchQueue.main.async {
-//                //                    //city Name displays as it was input by user
-//                //                    //cell.cityNameLabel?.text = self.cities[indexPath.row]
-//                //                    //city name displays as it is in JSON
-//                //                    print("INDEXPATH OUTPUT: \(indexPath.row)")
-//                //                    if indexPath.row < self.cityList.count {
-//                //                        cell.cityNameLabel?.text = self.cityList[indexPath.row].name
-//                //                        cell.descriptionLabel?.text = self.cityList[indexPath.row].desc
-//                //                        cell.temperatureLabel?.text = self.cityList[indexPath.row].temp
-//                //                        cell.iconImageView?.image = self.cityList[indexPath.row].icon
-//                // }
-//                //}
-//            }
-//        }
-//        session.resume()
+        print("COUNTRIES COUNT: \(countriesFromCoreData.count)")
+        print("CITIES ARRAYS COUNT: \(citiesFromCoreData.count)")
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
