edeeecbc3f2d76d1ab83d8c4a2bd1264f6c0e4be
A-n-n-a <alimanova1anna@gmail.com>
2017-07-11 16:03:35 +0300

    Intrface + Core Data added

--- FaiflyTest/ViewController.swift
+++ FaiflyTest/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var countries = [NSManagedObject]()
+    var country = NSManagedObject()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,51 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //let country = countries[indexPath.row]
+        let city = country.value(forKeyPath: "cities") as! [String]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = city[indexPath.row]
+        return cell
+    }
+    
+    func save(name: String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Country",
+                                       in: managedContext)!
+        
+        let country = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        country.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            countries.append(country)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
 
 
